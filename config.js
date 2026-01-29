8af922905888163e738354856ead906027285440
shlokbrahma-pixel <shlok.brahma@gmail.com>
2026-01-29 22:07:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝😘🥹😍💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        message: "Now come get a big warm hug and a huge kiss! And be ready for your gift!!😘",
         emojis: "🎁💖🤗💙😘❤️💕"  // These will bounce around
     },
 
